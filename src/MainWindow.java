7b91b75e10283a6c606a40680ba112e7312e9478
aroman07 <87022612+aroman07@users.noreply.github.com>
2022-10-26 11:50:06 +0300

    Add files via upload

---
+++ src/MainWindow.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow {
+    private JPanel panel;
+    private JButton buton;
+
+    public MainWindow() {
+        buton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //instructiuni care sa se execute la click
+                buton.setBackground(Color.GREEN);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Aplicatie");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new MainWindow().panel);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
